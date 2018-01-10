f62adf7a6bcc8713aa38b5ecbf98259615d742fa
loens_g <gregoire.loens@epitech.eu>
2018-01-10 17:41:49 +0100

    "sf::Window encapsulation with namespace"

--- sources/engine/display/Window.hpp
+++ sources/engine/display/Window.hpp
@@ -3,26 +3,32 @@
 
 # include <SFML/Window/Window.hpp>
 
+namespace Engine {
+    namespace Display {
+        class Window {
+        private:
+            sf::Window window;
 
-class Window {
-private:
-    sf::Window window;
+        public:
+            Window(unsigned int width, unsigned int height, std::string const &name) : window{
+                    sf::VideoMode(width, height),
+                    name} {}
 
-public:
-    Window(unsigned int width, unsigned int height, std::string const &name) : window{sf::VideoMode(width, height), name} {}
-    ~Window() = default;
+            ~Window() = default;
 
-public:
-    const sf::Window &Window::getWindow() const {return window;}
+        public:
+            const sf::Window &Window::getWindow() const { return window; }
 
-public:
-    bool isOpen() {return window.isOpen();}
-    bool pollEvent(sf::Event& event) {return window.pollEvent(event);}
-    void close() {window.close();}
+        public:
+            bool isOpen() { return window.isOpen(); }
 
-public:
-    Window() = delete;
-};
+            bool pollEvent(sf::Event &event) { return window.pollEvent(event); }
 
+            void close() { window.close(); }
 
+        public:
+            Window() = delete;
+        };
+    }
+}
 #endif //  !WINDOW_HPP
