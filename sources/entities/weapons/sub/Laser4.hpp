6a4ac1d1ffe8fc72b92e2bc60eb54c44a9c54b00
maxencedcx <maxence.delacroix@epitech.eu>
2018-01-17 10:47:28 +0100

    correct speed

--- sources/entities/weapons/sub/Laser4.hpp
+++ sources/entities/weapons/sub/Laser4.hpp
@@ -11,7 +11,7 @@ namespace Entities {
     class Laser4 : public AWeapon {
     public:
         Laser4(SCOPE *scope, uint64_t const &originId)
-                : AWeapon(scope, "resources/sprites/Lasers/laserRed11.png", 0.25, 5, 0, -0.25, originId) {}
+                : AWeapon(scope, "resources/sprites/Lasers/laserRed11.png", 0.25, 5, 0, -0.3, originId) {}
 
         virtual void shoot(std::vector<sf::Vector2f> const &canons, sf::Vector2f const &position);
 
