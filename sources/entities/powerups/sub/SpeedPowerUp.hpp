4fec0922e834e809d059573e0b1906b1acf6afdf
maxencedcx <maxence.delacroix@epitech.eu>
2018-01-19 13:24:43 +0100

    debug powerups + balance ships

--- sources/entities/powerups/sub/SpeedPowerUp.hpp
+++ sources/entities/powerups/sub/SpeedPowerUp.hpp
@@ -10,7 +10,7 @@ namespace Entities {
     class SpeedPowerUp : public APowerUp {
     public:
         SpeedPowerUp(SCOPE *scope, uint64_t id, GRADE grade, int x, int y)
-                : APowerUp(scope, id, isEnabled, x, y, 0, 0.1, grade) {
+                : APowerUp(scope, id, true, x, y, 0, 0.1, grade) {
             switch (grade) {
                 case BRONZE:
                     this->setTexture("resources/sprites/Power-ups/bolt_bronze.png");
--- sources/entities/ships/sub/_Wing.hpp
+++ sources/entities/ships/sub/_Wing.hpp
@@ -12,7 +12,7 @@ namespace Entities {
     public:
         _Wing(SCOPE *scope, uint64_t id = 0, bool isEnabled = true, const float &x = 0, const float &y = 0)
                 : PlayerShip(scope, id, "resources/sprites/ships/player/ship5/base.png", isEnabled, x, y, 0.15, 0.15,
-                             100) {
+                             150) {
             this->weapon = new Entities::Laser1(scope, this->team);
             this->canons.push_back({2, -15});
             this->canons.push_back({114, -15});
